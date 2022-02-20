bf02dd5c5c55609eaacdaf07c1334e0d49d28166
xaim-dev <yurabendzo20@gmail.com>
2022-02-20 13:00:00 +0200

    finished styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
-$(function(){
-
-    $('.your-class').slick({
-    })
+const sliderThumbs = new Swiper('.slider-thumbs', {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 3,
+    centeredSlides: true
 });
+
+sliderThumbs.on('click', (swiper) => {
+    swiper.slideTo(swiper.clickedIndex)
+})
+
+const sliderMain = new Swiper('.main__slider', {
+    loop: true,
+    loopedSlides: 4,
+});
+
+sliderThumbs.controller.control = sliderMain;
+sliderMain.controller.control = sliderThumbs;
